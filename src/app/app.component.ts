d11978140a448d1dcc68e4e3d14b0917f2eb1bf8
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-04-16 23:54:42 -0500

    [update] image products array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,51 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Alfredo';
   items = ['Alfredo', 'Ixchel', 'Porfirio'];
+
+  products = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    }
+  ];
   addItem():void {
     this.items.push('nuevo item perrón ');
   }
