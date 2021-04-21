90c0b8e43414c7a859f861d6b047aa93be43f935
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-04-21 10:24:46 -0500

    [Update] style Card component

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,13 +1,42 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Product } from '../product.model'
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChange,
+  OnInit,
+  DoCheck,
+  OnDestroy
+} from '@angular/core';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
   @Input() product: Product;
   @Output() productAddCart: EventEmitter<number> = new EventEmitter();
+  constructor() {
+    console.log('1. Constructor');
+  }
+
+  // ngOnChanges(change) {
+  //   console.log('2. OnChanges');
+  //   console.log(change);
+  // }
+  ngOnInit() {
+    console.log('3. OnInit');
+  }
+  ngDoCheck() {
+    console.log('4. DoCheck');
+  }
+  ngOnDestroy(){
+    console.log('5. OnDestroy');
+    
+  }
 
   addCart(): void {
     this.productAddCart.emit(this.product.id);
