cfd9a24d8e9015ee2380ce1370784f8c5069e559
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-04-21 10:29:06 -0500

    [Update ] cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {ProductComponent} from './components/product.component'
+import {ProductComponent} from './components/product.component';
+import { CartComponent } from './cart/cart.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
