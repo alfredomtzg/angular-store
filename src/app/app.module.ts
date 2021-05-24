a594acabcc52870a1aca603736ae60aac190baa8
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-05-23 23:45:20 -0500

    [Update] pipe Exponential

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {ProductComponent} from './components/product.component';
-import { CartComponent } from './cart/cart.component'
+import { CartComponent } from './cart/cart.component';
+import { ExponentialPipe } from './exponential.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    CartComponent
+    CartComponent,
+    ExponentialPipe
   ],
   imports: [
     BrowserModule,
