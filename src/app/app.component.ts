f92ab45cf1a76cc2912d13a57fb358a63ed146e1
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-05-23 23:41:22 -0500

    [Update]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,68 +1,67 @@
 import { Component } from '@angular/core';
 
-import {Product} from './product.model';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Alfredo';
   items = ['Alfredo', 'Ixchel', 'Porfirio'];
 
-  products: Product [] = [
+  products: Product[] = [
     {
       id: 1,
       image: 'assets/images/camiseta.png',
       title: 'Camiseta',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'bla bla bla bla bla',
     },
     {
       id: 2,
       image: 'assets/images/hoodie.png',
       title: 'Hoodie',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'bla bla bla bla bla',
     },
     {
       id: 3,
       image: 'assets/images/mug.png',
       title: 'Mug',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'bla bla bla bla bla',
     },
     {
       id: 4,
       image: 'assets/images/pin.png',
       title: 'Pin',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'bla bla bla bla bla',
     },
     {
       id: 5,
       image: 'assets/images/stickers1.png',
       title: 'Stickers',
       price: 80000,
-      description: 'bla bla bla bla bla'
+      description: 'bla bla bla bla bla',
     },
     {
       id: 6,
       image: 'assets/images/stickers2.png',
       title: 'Stickers',
       price: 80000,
-      description: 'bla bla bla bla bla'
-    }
+      description: 'bla bla bla bla bla',
+    },
   ];
-  addItem():void {
+  addItem(): void {
     this.items.push('nuevo item perrón ');
   }
   deleteItem(index: number): void {
-    this.items.splice(index, 1)
+    this.items.splice(index, 1);
   }
-  clickProduct(id: number){
-     console.log(id);
-     
+  clickProduct(id: number) {
+    console.log(id);
   }
 }
