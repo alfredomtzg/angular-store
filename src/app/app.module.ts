adfa4027f17e9b68639ddd9b28d34e842bf6ab35
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-05-24 23:06:23 -0500

    [Update] create header, banner and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,10 @@ import { HighlightDirective } from './highlight.directive';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
-import { DemoComponent } from './demo/demo.component'
+import { DemoComponent } from './demo/demo.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component'
 
 @NgModule({
   declarations: [
@@ -24,7 +27,10 @@ import { DemoComponent } from './demo/demo.component'
     HomeComponent,
     ProductsComponent,
     ContactComponent,
-    DemoComponent
+    DemoComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
