9054ca24e4861bf5e988a5a4a37966df418ce5dc
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-05-28 09:37:02 -0500

    [Update] create array

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
+  images: string []= [
+    'assets/images/banner-1.jpg',
+    'assets/images/banner-2.jpg',
+    'assets/images/banner-3.jpg',
+  ]
 
   constructor() { }
 
