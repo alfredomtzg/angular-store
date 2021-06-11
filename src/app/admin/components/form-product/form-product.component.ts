d387e11ac6392f42c079bf08977f73ea057b8612
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-06-10 22:14:42 -0500

    [Update] firebase init

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProductsService } from 'src/app/core/services/products/products.service';
 import { Router } from '@angular/router';
-import {MyValidator} from 'src/app/utils/validators'
+import { MyValidator } from 'src/app/utils/validators'
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -11,23 +14,45 @@ import {MyValidator} from 'src/app/utils/validators'
 })
 export class FormProductComponent implements OnInit {
   form: FormGroup;
+  image$: Observable<any>;
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
-    private productsService: ProductsService
+    private productsService: ProductsService,
+    private angularFireStorage: AngularFireStorage
   ) {
     this.buildForm();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
+
+  uploadFile(event: any) {
+    const file = event.target.files[0];
+    const name = 'images';
+    const fileRef = this.angularFireStorage.ref(name);
+    const task = this.angularFireStorage.upload(name, file);
+
+    task.snapshotChanges()
+    .pipe(
+      finalize(()=>{
+        this.image$ = fileRef.getDownloadURL();
+        this.image$.subscribe((url)=> {
+          console.log(url);
+          
+          this.form.get('image').setValue(url);
+        })
+      })
+    )
+    .subscribe();
+  }
 
   private buildForm() {
     this.form = this.formBuilder.group({
       id: ['', [Validators.required]],
       title: ['', [Validators.required]],
       price: [0, [Validators.required, MyValidator.isPriceValid]],
-      // image: ['', [Validators.required]],
+      image: [''],
       description: ['', [Validators.required]],
     });
   }
